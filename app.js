c45e6dd4c219f391da3059f9a8095e80e755d4d8
rahulp99 <priyadarshir99@gmail.com>
2021-09-23 19:05:00 +0530

    First

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+	res.send('Hello, Rahul!!')
+})
+
+app.listen(port, () => {
+	console.log('Listening at http://localhost:${port}')
+})
