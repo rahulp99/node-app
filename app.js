769ebabca3b771e3b9feef880423c14f42146263
rahulp99 <priyadarshir99@gmail.com>
2021-09-24 22:59:58 +0530

    Added html page

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 5000
 
+app.use(express.static('images'))
+
 app.get('/', (req, res) => {
-	res.send('Hello, Rahul!!')
+	res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 app.listen(port, () => {
